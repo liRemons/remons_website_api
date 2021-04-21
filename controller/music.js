914353f2cfe8c8e13cc70ebb63fbd1cb4ec3ca4d
liRemons <remons@foxmail.com>
2021-04-21 23:19:57 +0800

    udpate

--- controller/music.js
+++ controller/music.js
@@ -13,16 +13,7 @@ const search = async ({ sql }) => {
   result.data = res;
   return result;
 };
-// 获取歌曲列表
-const querySongList = async (ctx) => {
-  const { name, page } = REQ_ARG({ ctx, method: "GET" });
-  const { limitStart, limitEnd } = initPage({ page });
-  let sql = `SELECT id,name,authorId,collectionId,DATE_FORMAT(createTime,'%Y-%m-%d %H:%I:%S') AS createTime 
-              FROM music_list 
-              WHERE NAME LIKE '%${name}%' LIMIT ${limitStart},${limitEnd}`;
-  const result = await search({ sql });
-  ctx.body = result;
-};
+
 // 添加歌手
 const addSinger = async (ctx) => {
   const { name, photo } = REQ_ARG({ ctx, method: "POST" });
@@ -61,27 +52,141 @@ const delSinger = async (ctx) => {
   ctx.body = result;
 };
 // 上传歌手图片
-const uploadSingerPhoto = async(ctx) => {
+const uploadSingerPhoto = async (ctx) => {
   const file = ctx.request.files.file; // 获取上传文件
   const path = await upload({ file, folder: "music/singer" });
   ctx.body = {
     path,
   };
-}
-// 
-const uploadMusicFile = async(ctx) => {
+};
+
+// 获取专辑列表
+const queryCollectionList = async (ctx) => {
+  const { name } = REQ_ARG({ ctx, method: "GET" });
+  let sql = `SELECT id,name,photo,DATE_FORMAT(createTime,'%Y-%m-%d %H:%I:%S') AS createTime 
+              FROM music_collection
+              WHERE NAME LIKE '%${name || ""}%'`;
+  const result = await search({ sql });
+  ctx.body = result;
+};
+// 新增专辑
+const addCollection = async (ctx) => {
+  const { name } = REQ_ARG({ ctx, method: "POST" });
+  let sql = `INSERT INTO music_collection 
+    (id,name,photo,createTime) VALUES 
+    ('${uuid()}','${photo || ""}','${name}','${dateFormat()}')`;
+  const res = await query(sql);
+  ctx.body = initResult({});
+};
+// 更新专辑
+const updateCollection = async (ctx) => {
+  const { name, id, photo } = REQ_ARG({ ctx, method: "PUT" });
+  let sql = `update music_collection set 
+              name='${name}',
+              photo='${photo}',
+              createTime='${dateFormat()}' where id ='${id}'`;
+  const res = await query(sql);
+  ctx.body = initResult({});
+};
+// 删除专辑
+const delCollection = async (ctx) => {
+  const { ids } = REQ_ARG({ ctx, method: "DELETE" });
+  let sql = `delete from music_collection where id in ('${ids.join("','")}')`;
+  const res = await query(sql);
+  const result = initResult({});
+  ctx.body = result;
+};
+// 上传专辑图片
+const uploadCollectionPhoto = async (ctx) => {
   const file = ctx.request.files.file; // 获取上传文件
-  const path = await upload({ file, folder: "music/file" });
+  const path = await upload({ file, folder: "music/collection" });
   ctx.body = {
     path,
   };
-}
+};
 
-module.exports ={
+// 获取歌曲列表
+const querySongList = async (ctx) => {
+  const { name, page } = REQ_ARG({ ctx, method: "GET" });
+  const { limitStart, limitEnd } = initPage({ page });
+  // let sql = `SELECT id,name,authorId,collectionId,DATE_FORMAT(createTime,'%Y-%m-%d %H:%I:%S') AS createTime
+  //             FROM music_list
+  //             WHERE NAME LIKE '%${name}%' LIMIT ${limitStart},${limitEnd}`;
+  let sql = `select a.id, a.name, a.authorId, a.collectionId, b.name as authorName, c.name as collectionName
+  from music_list as a
+  left outer join music_singer as b on a.authorId = b.id
+  left outer join music_collection as c on a.collectionId = c.id`;
+  const result = await search({ sql });
+  ctx.body = result;
+};
+// 上传歌曲文件
+const uploadMusicFile = async (ctx) => {
+  const file = ctx.request.files.file; // 获取上传文件
+  const path = await upload({ file, folder: "music/song" });
+  ctx.body = {
+    path,
+  };
+};
+// 新增歌曲
+const addSong = async (ctx) => {
+  const { name, url, authorId, collectionId } = REQ_ARG({
+    ctx,
+    method: "POST",
+  });
+  let sql = `INSERT INTO music_list 
+    (
+      id,
+      name,
+      url,
+      authorId,
+      collectionId,
+      createTime
+    ) VALUES 
+    (
+      '${uuid()}',
+      '${name}',
+      '${url}',
+      '${authorId || ""}',
+      '${collectionId || ""}',
+      '${dateFormat()}'
+    )`;
+  const res = await query(sql);
+  ctx.body = initResult({});
+};
+const updateSong = async (ctx) => {
+  const { name, id, authorId, collectionId } = REQ_ARG({ ctx, method: "PUT" });
+  let sql = `update music_list set 
+  name='${name}',
+  createTime='${dateFormat()}',
+  authorId='${authorId}',
+  collectionId='${collectionId}'
+  where id ='${id}'`;
+  const res = await query(sql);
+  ctx.body = initResult({});
+};
+// 删除歌曲
+const delSong = async (ctx) => {
+  const { ids } = REQ_ARG({ ctx, method: "DELETE" });
+  let sql = `delete from music_list where id in ('${ids.join("','")}')`;
+  const res = await query(sql);
+  const result = initResult({});
+  ctx.body = result;
+};
+
+module.exports = {
   querySongList,
   addSinger,
   updateSinger,
   querySingerList,
   delSinger,
-  uploadSingerPhoto
-}
+  uploadSingerPhoto,
+  queryCollectionList,
+  addCollection,
+  updateCollection,
+  delCollection,
+  uploadCollectionPhoto,
+  uploadMusicFile,
+  addSong,
+  delSong,
+  updateSong,
+};
--- routes/music.js
+++ routes/music.js
@@ -7,13 +7,19 @@ const {
   querySingerList,
   delSinger,
   uploadSingerPhoto,
+  queryCollectionList,
+  addCollection,
+  updateCollection,
+  delCollection,
+  uploadCollectionPhoto,
   uploadMusicFile,
+  addSong,
+  delSong,
+  updateSong,
 } = require("../controller/music");
 
 router.prefix("/music");
 
-// 获取歌曲列表
-router.get("/querySongList", (ctx) => querySongList(ctx));
 // 添加歌手
 router.post("/addSinger", (ctx) => addSinger(ctx));
 // 修改歌手
@@ -24,7 +30,26 @@ router.get("/querySingerList", (ctx) => querySingerList(ctx));
 router.post("/uploadSingerPhoto", (ctx) => uploadSingerPhoto(ctx));
 // 删除歌手
 router.delete("/delSinger", (ctx) => delSinger(ctx));
+
+// 获取专辑列表
+router.get("/queryCollectionList", (ctx) => queryCollectionList(ctx));
+// 新增专辑
+router.post("/addCollection", (ctx) => addCollection(ctx));
+// 更新专辑
+router.put("/updateCollection", (ctx) => updateCollection(ctx));
+// 删除专辑
+router.delete("/delCollection", (ctx) => delCollection(ctx));
+// 上传专辑图片
+router.post("/uploadCollectionPhoto", (ctx) => uploadCollectionPhoto(ctx));
+
+// 获取歌曲列表
+router.get("/querySongList", (ctx) => querySongList(ctx));
+// 新增歌曲
+router.post("/addSong", (ctx) => addSong(ctx));
+// 更新歌曲
+router.put("/updateSong", (ctx) => updateSong(ctx));
+// 删除歌曲
+router.delete("/delSong", (ctx) => delSong(ctx));
 // 歌曲上传
 router.post("/uploadMusicFile", (ctx) => uploadMusicFile(ctx));
-
 module.exports = router;
