8f12f569d33b737948e6305d8f790b9ace7fa9ba
liRemons <remons@foxmail.com>
2021-04-24 08:55:49 +0800

    add content

--- controller/content.js
+++ controller/content.js
@@ -10,13 +10,66 @@ const {
   REQ_ARG,
 } = require("../utils");
 
+const search = async ({ sql }) => {
+  const result = initResult({});
+  const res = await query(sql);
+  result.data = res;
+  return result;
+};
+
+// 查询技术分类
+const queryTechClassList = async (ctx) => {
+  const { name } = REQ_ARG({ ctx, method: "GET" });
+  let sql = `SELECT id,name,icon,DATE_FORMAT(createTime,'%Y-%m-%d %H:%I:%S') AS createTime 
+              FROM tech_class
+              WHERE NAME LIKE '%${name || ""}%'`;
+  const result = await search({ sql });
+  ctx.body = result;
+};
+// 新增技术分类
+const addTechClass = async (ctx) => {
+  const { name, icon } = REQ_ARG({ ctx, method: "POST" });
+  let sql = `INSERT INTO tech_class 
+    (id,name,icon,createTime) VALUES 
+    ('${uuid()}','${name}','${icon||''}','${dateFormat()}')`;
+  const res = await query(sql);
+  ctx.body = initResult({});
+};
+// 上传技术 icon
+const uploadTechClassIcon = async (ctx) => {
+  const file = ctx.request.files.file; // 获取上传文件
+  const path = await upload({ file, folder: "content/icon" });
+  ctx.body = {
+    path,
+  };
+};
+// 更新技术分类
+const updateTechClass = async (ctx) => {
+  const { name, id, icon } = REQ_ARG({ ctx, method: "PUT" });
+  let sql = `update tech_class set 
+              name='${name}',
+              icon='${icon}',
+              createTime='${dateFormat()}' where id ='${id}'`;
+  const res = await query(sql);
+  ctx.body = initResult({});
+};
+// 删除技术分类
+const delTechClass = async (ctx) => {
+  const { ids } = REQ_ARG({ ctx, method: "DELETE" });
+  let sql = `delete from tech_class where id in ('${ids.join("','")}')`;
+  const res = await query(sql);
+  const result = initResult({});
+  ctx.body = result;
+};
+
+// 创建markdown文件
 const createMarkdown = async ({ content, folder }) => {
   fs.writeFile(path.join(__dirname, "../upload/" + folder), content, (err) => {
     if (err) throw err;
     return "/upload/" + folder;
   });
 };
-
+// 添加文章
 const addArticle = async (ctx) => {
   const { content } = REQ_ARG({ ctx, method: "POST" });
   const pathname = await createMarkdown({
@@ -27,5 +80,10 @@ const addArticle = async (ctx) => {
 };
 
 module.exports = {
+  queryTechClassList,
+  addTechClass,
+  uploadTechClassIcon,
+  updateTechClass,
+  delTechClass,
   addArticle,
 };
--- routes/content.js
+++ routes/content.js
@@ -1,7 +1,25 @@
 const router = require("koa-router")();
-const { addArticle } = require("../controller/content");
+const {
+  queryTechClassList,
+  addTechClass,
+  uploadTechClassIcon,
+  updateTechClass,
+  delTechClass,
+  addArticle,
+} = require("../controller/content");
 router.prefix("/content");
 
+// 查询技术分类
+router.get("/queryTechClassList", (ctx) => queryTechClassList(ctx));
+// 新增技术分类
+router.post("/addTechClass", (ctx) => addTechClass(ctx));
+// 上传技术icon
+router.post("/uploadTechClassIcon", (ctx) => uploadTechClassIcon(ctx));
+// 更新技术分类
+router.put("/updateTechClass", (ctx) => updateTechClass(ctx));
+// 删除技术分类
+router.delete("/delTechClass", (ctx) => delTechClass(ctx));
+// 新增文章
 router.post("/addArticle", (ctx) => addArticle(ctx));
 
 module.exports = router;
