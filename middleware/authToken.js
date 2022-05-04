cdf34059598d7108d087ebf82813b98c63a51def
liRemons <remons@foxmail.com>
2022-05-04 17:46:24 +0800

    test

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -6,6 +6,7 @@ const authToken = async (ctx, next) => {
     await next();
     return;
   }
+  console.log(ctx.method.toLowerCase(), 'ctx.method.toLowerCase()');
   if (ctx.method.toLowerCase() !== 'get') {
     if (ctx.header.remons_token === 'null') {
       ctx.body = {
